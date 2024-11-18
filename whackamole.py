a566432eca02166d6938a15e056fb0b559180835
cpetruzzelli <c.petruzzelli@ufl.edu>
2024-11-18 00:42:10 -0500

    Add files via upload
    Collin Petruzzelli whackamole.py for Lab 10

--- whackamole.py
+++ whackamole.py
@@ -1,20 +1,35 @@
 import pygame
+import random
 
 
 def main():
     try:
         pygame.init()
-        # You can draw the mole with this snippet:
-        # screen.blit(mole_image, mole_image.get_rect(topleft=(x,y)))
-        mole_image = pygame.image.load("mole.png")
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
+        mole_image = pygame.image.load("mole.png")
+        mole_size = 32
+        mole_x, mole_y = 0, 0
         running = True
+
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_x, mouse_y = event.pos
+                    if mole_x <= mouse_x < mole_x + mole_size and mole_y <= mouse_y < mole_y + mole_size:
+                        mole_x = random.randrange(0, 20) * mole_size
+                        mole_y = random.randrange(0, 16) * mole_size
+
             screen.fill("light green")
+
+            for x in range(0, 640, mole_size):
+                pygame.draw.line(screen, "white", (x, 0), (x, 512))
+            for y in range(0, 512, mole_size):
+                pygame.draw.line(screen, "white", (0, y), (640, y))
+
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mole_x, mole_y)))
             pygame.display.flip()
             clock.tick(60)
     finally:
@@ -23,3 +38,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
